dc7ada245a424152352afe2163e997d8698096ff
Ken Yourek <ken.yourek@microlution-inc.com>
2014-09-12 11:09:11 -0500

    Bump version.

--- sln/Pagination/Properties/AssemblyInfo.cs
+++ sln/Pagination/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: Guid("ce155db1-e4da-4e59-a151-7f47d3ac1f8c")]
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.3.24")]
+[assembly: AssemblyFileVersion("1.0.3.25")]
 [assembly: AssemblyInformationalVersion("1.0.3")]
 
 [assembly: InternalsVisibleTo("Pagination.Tests")]
