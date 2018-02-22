464d053e8ffdb4dcd54da2d7db22cd3fc969531b
Ken Yourek <ken@yourek.net>
2018-02-22 12:29:24 -0600

    Add git commands to tool.

--- sln/Pagination.Release/ReleaseActions/Tag.cs
+++ sln/Pagination.Release/ReleaseActions/Tag.cs
@@ -2,7 +2,9 @@
     class Tag : ReleaseAction {
         public override void Work() {
             var tag = Context.Version.StagedVersion;
+            Process("git", "status");
             Process("git", "commit", "-a", "-m", $"\"(Auto-)Commit version '{tag}'.\"");
+            Process("git", "push");
             Process("git", "tag", "-a", tag, "-m", $"\"(Auto-)Tag version '{tag}'.\"");
             Process("git", "push", "origin", tag);
         }
