5c9bde6a2b5c3f84dbefd12d9b3c48a8124ca150
Ken Yourek <ken@yourek.net>
2020-03-24 07:39:35 -0500

    Switch to SDK project for sample.

--- sln/Pagination.Sample/Properties/AssemblyInfo.cs
+++ sln/Pagination.Sample/Properties/AssemblyInfo.cs
@@ -1,10 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyCompany("Ken Yourek")]
-[assembly: AssemblyCopyright("Copyright © Ken Yourek 2013-2020")]
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyTitle("Pagination.Sample")]
-[assembly: AssemblyDescription("Pagination Sample")]
-[assembly: AssemblyProduct("Pagination.Sample")]
